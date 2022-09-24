fc1ca399202f1423bc31c48892b68f30279d0c84
Nikhil Barthwal <nikhilbarthwal@hotmail.com>
2022-09-24 10:03:40 -0700

    cleanup

--- eventing/translation/csharp/Program.cs
+++ eventing/translation/csharp/Program.cs
@@ -1,4 +1,4 @@
-﻿// Copyright 2019 Google LLC
+// Copyright 2019 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -16,7 +16,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
 namespace translation
-{    
+{
     public class Program
     {
         public static void Main(string[] args)
--- eventing/translation/csharp/Startup.cs
+++ eventing/translation/csharp/Startup.cs
@@ -1,4 +1,4 @@
-﻿// Copyright 2019 Google LLC
+// Copyright 2019 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -102,7 +102,7 @@ namespace translation
                 throw new ArgumentException("Translation request cannot be null");
             }
 
-            if (string.IsNullOrEmpty(translationRequest.Text)) 
+            if (string.IsNullOrEmpty(translationRequest.Text))
             {
                 throw new ArgumentException("Translation text cannot be empty or null");
             }
